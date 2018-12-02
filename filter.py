988beaacc4a1266d0b9468bd483ff1300635fad3
jjarrett9 <31669078+jjarrett9@users.noreply.github.com>
2018-12-02 18:39:21 -0500

    Peak and shelfs
    filter coeffs entered

--- filter.py
+++ filter.py
@@ -12,13 +12,13 @@ class Filter:
     def __init__ (self):
         self.x1 = self.x2 = self.y1 = self.y2 = 0
     
-    def filter (self, x, fs, f0, q, type):
+    def filter (self, x, fs, f0, q, type, dbGain):
     
         #initializing return array
         y = np.zeros(x.shape, x.dtype)
         
         
-        coeffs = self.getCoeffs(f0, fs, q, type)
+        coeffs = self.getCoeffs(f0, fs, q, type, dbGain)
         
         #transfer functions
         for ch in range(0, x.shape[1]):
@@ -34,11 +34,12 @@ class Filter:
             
         return y
     
-    def getCoeffs (self, f0, fs, q, type):
+    def getCoeffs (self, f0, fs, q, type, dBgain):
         w = 2*math.pi*(f0/fs)
         cw = math.cos(w)
         sw = math.sin(w)
         alpha = sw/(2*q) 
+        bigA  = 10 ^(dBgain/40)
         
         #filter coefficients from trig wizardry
         
@@ -79,7 +80,34 @@ class Filter:
             a0 =   1 + alpha
             a1 =  -2*cw
             a2 =   1 - alpha 
-    
+
+        elif type is Filter.PEAK:
+
+            b0 = 1 + alpha*bigA
+            b1 = -2*cw
+            b2 = 1-alpha*bigA
+            a0 = 1 + alpha/bigA
+            a1 = -2*cw
+            a2 = 1-alpha/bigA
+
+        elif type is Filter.LOWSHELF:
+
+            b0 = bigA * ((bigA + 1) + (bigA - 1) * cw + 2 * math.sqrt(bigA)*alpha)
+            b1 = 2 * bigA * ((bigA - 1) - (bigA + 1) * cw)
+            b2 = bigA * ((bigA + 1) - (bigA - 1) * cw - 2*math.sqrt(bigA) * alpha)
+            a0 = (bigA + 1) + (bigA - 1) * cw + 2 * math.sqrt(bigA) * alpha
+            a1 = -2 * ((bigA - 1) + (bigA + 1) * cw)
+            a2 = (bigA + 1) + (bigA - 1) * cw - 2*math.sqrt(bigA) * alpha
+
+        elif type is Filter.HIGHSHELF:
+
+            b0 = bigA * ((bigA + 1) + (bigA - 1) * cw + 2 * math.sqrt(bigA)*alpha)
+            b1 = -2 * bigA * ((bigA - 1) + (bigA + 1) * cw)
+            b2 = bigA * ((bigA + 1) - (bigA - 1) * cw - 2*math.sqrt(bigA) * alpha)
+            a0 = (bigA + 1) - (bigA - 1) * cw + 2 * math.sqrt(bigA) * alpha
+            a1 = 2 * ((bigA - 1) - (bigA + 1) * cw)
+            a2 = (bigA + 1) - (bigA - 1) * cw - 2*math.sqrt(bigA) * alpha
+
         b0 /= a0
         b1 /= a0
         b2 /= a0
